58c7a022c37d8e4c51f67e1d54cbd43b00642078
JaceLehman228 <jacobthomaslehman@gmail.com>
2024-08-08 14:38:03 -0700

    Dragons Exercise Complete

--- all-dragons.js
+++ all-dragons.js
@@ -1,12 +1,19 @@
 // Your code here 
+const evilDragon = require('../aa04-class-dragons/classes/evil-dragon');
+const friendlyDragon = require('../aa04-class-dragons/classes/friendly-dragon');
+const Dragon = require('./classes/dragon');
 
+let falkor = new friendlyDragon('Falkor', 'white', ["save Atreyu from the swamp", "save Atreyu from the Nothing", "scare the local bullies into a dumpster"], 'Bastian');
+let smaug = new evilDragon('Smaug', 'black', ["take over your mountain kingdom", "steal all your dwarven gold", "burn down your floating village"], 'Dwarf King');
 
+let allDragons = Dragon.getDragons(falkor, smaug);
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
 try {
   module.exports.falkor = falkor
   module.exports.smaug = smaug
+  module.exports.allDragons = allDragons
 } catch {
   module.exports = null;
 }
--- classes/dragon.js
+++ classes/dragon.js
@@ -1,5 +1,25 @@
 // Your code here 
+class Dragon {
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
+  }
+  breathesFire() {
+    return `${this.name} breathes fire everywhere! BURN!!!!`;
+  }
 
+  static getDragons(...dragons) {
+    let nameArr = []
+    for(let i = 0; i < dragons.length; i++) {
+      nameArr.push(dragons[i].name);
+    }
+    return nameArr;
+  }
+}
+
+// const puff = new Dragon("Puff", "green");
+// console.log(puff);
+// console.log(puff.breathesFire());
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
